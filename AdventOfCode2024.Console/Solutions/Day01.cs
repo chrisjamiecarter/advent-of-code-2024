9075ac58b03027fdf9a3bdca71587e790efb21d5
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-12-01 13:45:34 +0000

    Added day 1 part 1 problem

---
+++ AdventOfCode2024.Console/Solutions/Day01.cs
@@ -0,0 +1,23 @@
+﻿using AoCHelper;
+
+namespace AdventOfCode2024.Console.Solutions;
+
+public class Day01 : BaseDay
+{
+    private readonly string _input;
+
+    public Day01()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    }
+}
