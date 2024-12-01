238b00787521ec4136c4cad7edb03555b855e3a9
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-12-01 14:26:27 +0000

    Day one complete

--- AdventOfCode2024.Console/Solutions/Day01.cs
+++ AdventOfCode2024.Console/Solutions/Day01.cs
@@ -13,11 +13,46 @@ public class Day01 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+        var answer = "TODO";
+
+        List<int> left = [];
+        List<int> right = [];
+
+        foreach (var line in _input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+        {
+            var numbers = line.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            left.Add(Convert.ToInt32(numbers.First()));
+            right.Add(Convert.ToInt32(numbers.Last()));
+        }
+
+        left.Sort();
+        right.Sort();
+
+        var differences = left.Zip(right, (x, y) => Math.Abs(x - y));
+
+        answer = differences.Sum().ToString();
+
+        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1 = '{answer}'");
     }
 
     public override ValueTask<string> Solve_2()
     {
-        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        var answer = "TODO";
+
+        List<int> left = [];
+        List<int> right = [];
+
+        foreach (var line in _input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+        {
+            var numbers = line.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            left.Add(Convert.ToInt32(numbers.First()));
+            right.Add(Convert.ToInt32(numbers.Last()));
+        }
+
+        var similarityScores = left.Select(x => x * right.Count(y => x == y));
+
+        answer = similarityScores.Sum().ToString();
+
+        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 = '{answer}'");
     }
 }
