fa69c2b7b3129b74138efc0f0d6c7c6e558ff9de
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-12-02 15:40:31 +0000

    Part two complete

--- AdventOfCode2024.Console/Solutions/Day02.cs
+++ AdventOfCode2024.Console/Solutions/Day02.cs
@@ -21,33 +21,9 @@ public class Day02 : BaseDay
         {
             var numbers = line.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(x => Convert.ToInt32(x)).ToList();
             
-            var isAllIncreasing = true;
-            for (int i = 0; i < numbers.Count; i++)
-            {
-                if (i != 0)
-                {
-                    int difference = numbers[i] - numbers[i - 1];
-                    if (difference < 1 || difference > 3)
-                    {
-                        isAllIncreasing = false;
-                        break;
-                    }
-                }
-            }
+            var isAllIncreasing = IsAllIncreasing(numbers);
 
-            var isAllDecreasing = true;
-            for (int i = 0; i < numbers.Count; i++)
-            {
-                if (i != 0)
-                {
-                    int difference = numbers[i - 1] - numbers[i];
-                    if (difference < 1 || difference > 3)
-                    {
-                        isAllDecreasing = false;
-                        break;
-                    }
-                }
-            }
+            var isAllDecreasing = IsAllDecreasing(numbers);
 
             if (isAllIncreasing || isAllDecreasing)
             {
@@ -64,6 +40,84 @@ public class Day02 : BaseDay
     {
         var answer = "TODO";
 
+        int safeReports = 0;
+
+        foreach (var line in _input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+        {
+            var numbers = line.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(x => Convert.ToInt32(x)).ToList();
+
+            var isAllIncreasing = IsAllIncreasingWithProblemDampening(numbers);
+
+            var isAllDecreasing = IsAllDecreasingWithProblemDampening(numbers);
+            
+            if (isAllIncreasing || isAllDecreasing)
+            {
+                safeReports++;
+            }
+        }
+
+        answer = safeReports.ToString();
+
         return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 = '{answer}'");
+    }
+
+    private static bool IsAllDecreasing(List<int> numbers)
+    {
+        for (int j = 0; j < numbers.Count; j++)
+        {
+            if (j != 0)
+            {
+                int difference = numbers[j - 1] - numbers[j];
+                if (difference < 1 || difference > 3)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static bool IsAllDecreasingWithProblemDampening(List<int> numbers)
+    {
+        List<bool> outcomes = [];
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            var dampenedNumbers = numbers.ToList();
+            dampenedNumbers.RemoveAt(i);
+            outcomes.Add(IsAllDecreasing(dampenedNumbers));
+        }
+
+        return outcomes.Any(x => x);
+    }
+
+    private static bool IsAllIncreasing(List<int> numbers)
+    {
+        for (int j = 0; j < numbers.Count; j++)
+        {
+            if (j != 0)
+            {
+                int difference = numbers[j] - numbers[j - 1];
+                if (difference < 1 || difference > 3)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static bool IsAllIncreasingWithProblemDampening(List<int> numbers)
+    {
+        List<bool> outcomes = [];
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            var dampenedNumbers = numbers.ToList();
+            dampenedNumbers.RemoveAt(i);
+            outcomes.Add(IsAllIncreasing(dampenedNumbers));
+        }
+
+        return outcomes.Any(x => x);
     }
 }
