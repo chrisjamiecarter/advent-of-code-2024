4a5bb73770188d29d9194ebfcae5eb7bece19a04
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-12-05 10:32:13 +0000

    Day five part one complete

--- src/AdventOfCode2024/Solutions/Day05.cs
+++ src/AdventOfCode2024/Solutions/Day05.cs
@@ -15,11 +15,43 @@ public class Day05 : BaseDay
     {
         var answer = string.Empty;
 
-        // SOLVE.
+        var middlePageNumbersSum = 0;
 
-        answer = "TODO";
+        var pageOrderRules = _input.Split($"{Environment.NewLine}{Environment.NewLine}").First();
+        var pageUpdates = _input.Split($"{Environment.NewLine}{Environment.NewLine}").Last();
+
+        foreach (var update in pageUpdates.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+        {
+            if (CheckRules(update, pageOrderRules))
+            {
+                var pageNumbers = update.Split(',');
+                var middleIndex = (pageNumbers.Length / 2);
+                var middleNumber = pageNumbers[middleIndex];
+                middlePageNumbersSum += Convert.ToInt32(middleNumber);
+            }            
+        }
+
+        answer = middlePageNumbersSum.ToString();
 
         return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1 = '{answer}'");
+    }
+
+    private static bool CheckRules(string pageUpdate, string pageOrderRules)
+    {
+        foreach (var rule in pageOrderRules.Split(Environment.NewLine))
+        {
+            var x = rule.Split('|').First();
+            var y = rule.Split('|').Last();
+            var xIndex = pageUpdate.IndexOf(x);
+            var yIndex = pageUpdate.IndexOf(y);
+
+            if ((xIndex != -1 && yIndex != -1) && xIndex >= yIndex)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public override ValueTask<string> Solve_2()
