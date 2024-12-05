a47aa11d4eacce163697c1732c2a4046ab5d5529
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-12-05 14:38:32 +0000

    Day five part two complete

--- src/AdventOfCode2024/Solutions/Day05.cs
+++ src/AdventOfCode2024/Solutions/Day05.cs
@@ -6,9 +6,27 @@ public class Day05 : BaseDay
 {
     private readonly string _input;
 
+    private readonly Dictionary<int, List<int>> _pageOrderRules;
+
+    private readonly List<List<int>> _pageUpdates;
+
     public Day05()
     {
         _input = File.ReadAllText(InputFilePath);
+
+        _pageOrderRules = _input.Split($"{Environment.NewLine}{Environment.NewLine}")
+            .First()
+            .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+            .Select(x => x.Split('|').Select(int.Parse).ToList())
+            .Select(x => (x[0], x[1]))
+            .GroupBy(x => x.Item1)
+            .ToDictionary(x => x.Key, x => x.Select(x => x.Item2).Order().ToList());
+
+        _pageUpdates = _input.Split($"{Environment.NewLine}{Environment.NewLine}")
+            .Last()
+            .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+            .Select(x => x.Split(',').Select(int.Parse).ToList())
+            .ToList();
     }
 
     public override ValueTask<string> Solve_1()
@@ -17,18 +35,12 @@ public class Day05 : BaseDay
 
         var middlePageNumbersSum = 0;
 
-        var pageOrderRules = _input.Split($"{Environment.NewLine}{Environment.NewLine}").First();
-        var pageUpdates = _input.Split($"{Environment.NewLine}{Environment.NewLine}").Last();
-
-        foreach (var update in pageUpdates.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+        foreach (var update in _pageUpdates)
         {
-            if (CheckRules(update, pageOrderRules))
+            if (IsUpdateValid(update, _pageOrderRules))
             {
-                var pageNumbers = update.Split(',');
-                var middleIndex = (pageNumbers.Length / 2);
-                var middleNumber = pageNumbers[middleIndex];
-                middlePageNumbersSum += Convert.ToInt32(middleNumber);
-            }            
+                middlePageNumbersSum += update[update.Count / 2];
+            }
         }
 
         answer = middlePageNumbersSum.ToString();
@@ -36,32 +48,71 @@ public class Day05 : BaseDay
         return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1 = '{answer}'");
     }
 
-    private static bool CheckRules(string pageUpdate, string pageOrderRules)
+    public override ValueTask<string> Solve_2()
     {
-        foreach (var rule in pageOrderRules.Split(Environment.NewLine))
-        {
-            var x = rule.Split('|').First();
-            var y = rule.Split('|').Last();
-            var xIndex = pageUpdate.IndexOf(x);
-            var yIndex = pageUpdate.IndexOf(y);
+        var answer = string.Empty;
 
-            if ((xIndex != -1 && yIndex != -1) && xIndex >= yIndex)
+        var middlePageNumbersSum = 0;
+
+        foreach (var update in _pageUpdates)
+        {
+            if (!IsUpdateValid(update, _pageOrderRules))
             {
-                return false;
+                while (!IsUpdateValid(update, _pageOrderRules))
+                {
+                    ReorderUpdate(update, _pageOrderRules);
+                }
+                middlePageNumbersSum += update[update.Count / 2];
+            }
+        }
+
+        answer = middlePageNumbersSum.ToString();
+
+        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 = '{answer}'");
+    }
+
+    private static bool IsPageOrderValid(Dictionary<int, List<int>> rules, int page, int comparisionPage)
+    {
+        return !rules.TryGetValue(comparisionPage, out var subsquentPages) || !subsquentPages.Contains(page);
+    }
+
+    private static bool IsUpdateValid(List<int> update, Dictionary<int, List<int>> rules)
+    {
+        for (int i = 0; i < update.Count; i++)
+        {
+            var page = update[i];
+
+            for (var j = i + 1; j < update.Count; j++)
+            {
+                var comparisionPage = update[j];
+
+                if (!IsPageOrderValid(rules, page, comparisionPage))
+                {
+                    return false;
+                }
             }
         }
 
         return true;
     }
 
-    public override ValueTask<string> Solve_2()
+    private static void ReorderUpdate(List<int> update, Dictionary<int, List<int>> rules)
     {
-        var answer = string.Empty;
+        for (var i = 0; i < update.Count; i++)
+        {
+            var page = update[i];
 
-        // SOLVE.
+            for (var j = i + 1; j < update.Count; j++)
+            {
+                var comparisionPage = update[j];
 
-        answer = "TODO";
-
-        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 = '{answer}'");
+                if (!IsPageOrderValid(rules, page, comparisionPage))
+                {
+                    update[i] = comparisionPage;
+                    update[j] = page;
+                    return;
+                }
+            }
+        }
     }
 }
