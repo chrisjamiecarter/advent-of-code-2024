00dbf086fe7b43d285c0905fdafac658b73395b9
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-12-10 12:09:49 +0000

    Day nine part one complete

--- src/AdventOfCode2024/Solutions/Day09.cs
+++ src/AdventOfCode2024/Solutions/Day09.cs
@@ -6,9 +6,28 @@ public class Day09 : BaseDay
 {
     private readonly string _input;
 
+    private List<int?> _fileMap = [];
+
     public Day09()
     {
         _input = File.ReadAllText(InputFilePath);
+
+        var index = 0;
+        for (int i = 0; i < _input.Length; i++)
+        {
+            var length = int.Parse(_input[i].ToString());
+            int? value = null;
+            if(i % 2 == 0)
+            {
+                value = index;
+                index++;
+            }
+
+            for (int j = 0; j < length; j++)
+            {
+                _fileMap.Add(value);
+            }
+        }
     }
 
     public override ValueTask<string> Solve_1()
@@ -16,8 +35,31 @@ public class Day09 : BaseDay
         var answer = string.Empty;
 
         // SOLVE.
+        var nulls = _fileMap.Count(x => !x.HasValue);
+        for (int i = 0; i < nulls; i++)
+        {
+            var firstIndex = _fileMap.FindIndex(x => !x.HasValue);
+            var lastIndex = _fileMap.FindLastIndex(x => x.HasValue);
+            int? firstValue = _fileMap[firstIndex];
+            int? lastValue = _fileMap[lastIndex];
+            _fileMap[firstIndex] = lastValue;
+            _fileMap[lastIndex] = firstValue;
+        }
 
-        answer = "TODO";
+        ulong filesystemChecksum = 0;
+        for (int i = 0; i < _fileMap.Count; i++)
+        {
+            if (_fileMap[i].HasValue)
+            {
+                filesystemChecksum += (ulong)(i * _fileMap[i]!.Value);
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        answer = filesystemChecksum.ToString();
 
         return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1 = '{answer}'");
     }
