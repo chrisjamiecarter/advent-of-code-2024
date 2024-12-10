c9c43fb72e7cd1efbae53148d8dfbaa620911c72
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-12-10 13:04:27 +0000

    Day nine part two complete

--- src/AdventOfCode2024/Solutions/Day09.cs
+++ src/AdventOfCode2024/Solutions/Day09.cs
@@ -6,18 +6,21 @@ public class Day09 : BaseDay
 {
     private readonly string _input;
 
-    private List<int?> _fileMap = [];
-
     public Day09()
     {
         _input = File.ReadAllText(InputFilePath);
+    }
+
+    private List<int?> GetFileMap()
+    {
+        List<int?> fileMap = [];
 
         var index = 0;
         for (int i = 0; i < _input.Length; i++)
         {
             var length = int.Parse(_input[i].ToString());
             int? value = null;
-            if(i % 2 == 0)
+            if (i % 2 == 0)
             {
                 value = index;
                 index++;
@@ -25,33 +28,37 @@ public class Day09 : BaseDay
 
             for (int j = 0; j < length; j++)
             {
-                _fileMap.Add(value);
+                fileMap.Add(value);
             }
         }
+
+        return fileMap;
     }
 
     public override ValueTask<string> Solve_1()
     {
         var answer = string.Empty;
 
+        var fileMap = GetFileMap();
+
         // SOLVE.
-        var nulls = _fileMap.Count(x => !x.HasValue);
+        var nulls = fileMap.Count(x => !x.HasValue);
         for (int i = 0; i < nulls; i++)
         {
-            var firstIndex = _fileMap.FindIndex(x => !x.HasValue);
-            var lastIndex = _fileMap.FindLastIndex(x => x.HasValue);
-            int? firstValue = _fileMap[firstIndex];
-            int? lastValue = _fileMap[lastIndex];
-            _fileMap[firstIndex] = lastValue;
-            _fileMap[lastIndex] = firstValue;
+            var firstIndex = fileMap.FindIndex(x => !x.HasValue);
+            var lastIndex = fileMap.FindLastIndex(x => x.HasValue);
+            int? firstValue = fileMap[firstIndex];
+            int? lastValue = fileMap[lastIndex];
+            fileMap[firstIndex] = lastValue;
+            fileMap[lastIndex] = firstValue;
         }
 
         ulong filesystemChecksum = 0;
-        for (int i = 0; i < _fileMap.Count; i++)
+        for (int i = 0; i < fileMap.Count; i++)
         {
-            if (_fileMap[i].HasValue)
+            if (fileMap[i].HasValue)
             {
-                filesystemChecksum += (ulong)(i * _fileMap[i]!.Value);
+                filesystemChecksum += (ulong)(i * fileMap[i]!.Value);
             }
             else
             {
@@ -68,9 +75,83 @@ public class Day09 : BaseDay
     {
         var answer = string.Empty;
 
-        // SOLVE.
+        var fileMap = GetFileMap();
 
-        answer = "TODO";
+        // SOLVE.
+        for (int i = fileMap.Count - 1; i >= 0; i--)
+        {
+            if (fileMap[i].HasValue)
+            {
+                int compare = fileMap[i]!.Value;
+                List<int> firstIndexes = [];
+                List<int> lastIndexes = [];
+                int tempIndex = i;
+                while (tempIndex >= 0 && fileMap[tempIndex].HasValue && fileMap[tempIndex]!.Value == compare)
+                {
+                    lastIndexes.Add(tempIndex);
+                    tempIndex--;
+                    i = tempIndex + 1;
+                }
+
+                if (!lastIndexes.Any())
+                {
+                    continue;
+                }
+
+                List<int> compareIndexes = [];
+                for (int j = 0; j < fileMap.Count; j++)
+                {
+                    if (j > i)
+                    {
+                        compareIndexes = [];
+                        break;
+                    }
+
+                    if (!fileMap[j].HasValue)
+                    {
+                        compareIndexes.Add(j);
+                    }
+                    else
+                    {
+                        if (lastIndexes.Count <= compareIndexes.Count)
+                        {
+                            firstIndexes = compareIndexes;
+                            break;
+                        }
+                        else
+                        {
+                            compareIndexes = [];
+                        }    
+                    }
+                }
+
+                if (!firstIndexes.Any())
+                {
+                    continue;
+                }
+
+                for (int k = 0; k < lastIndexes.Count; k++)
+                {
+                    var firstIndex = firstIndexes[k];
+                    var lastIndex = lastIndexes[k];
+                    int? firstValue = fileMap[firstIndex];
+                    int? lastValue = fileMap[lastIndex];
+                    fileMap[firstIndex] = lastValue;
+                    fileMap[lastIndex] = firstValue;
+                }
+            }
+        }
+
+        ulong filesystemChecksum = 0;
+        for (int i = 0; i < fileMap.Count; i++)
+        {
+            if (fileMap[i].HasValue)
+            {
+                filesystemChecksum += (ulong)(i * fileMap[i]!.Value);
+            }
+        }
+
+        answer = filesystemChecksum.ToString();
 
         return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 = '{answer}'");
     }
