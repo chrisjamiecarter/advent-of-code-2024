3d018f23722ae5a9574bfc06c30d888e2fce524f
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-12-11 11:43:49 +0000

    Completed day eleven part one

--- src/AdventOfCode2024/Solutions/Day11.cs
+++ src/AdventOfCode2024/Solutions/Day11.cs
@@ -6,9 +6,12 @@ public class Day11 : BaseDay
 {
     private readonly string _input;
 
+    private readonly ulong[] _numbers;
+
     public Day11()
     {
         _input = File.ReadAllText(InputFilePath);
+        _numbers = _input.Split(' ').Select(ulong.Parse).ToArray();
     }
 
     public override ValueTask<string> Solve_1()
@@ -16,8 +19,14 @@ public class Day11 : BaseDay
         var answer = string.Empty;
 
         // SOLVE.
+        var numbers = _numbers.ToArray();
 
-        answer = "TODO";
+        for (int i = 0; i < 25; i++)
+        {
+            numbers = numbers.SelectMany(Blink).ToArray();
+        }
+
+        answer = numbers.Length.ToString();
 
         return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1 = '{answer}'");
     }
@@ -31,5 +40,22 @@ public class Day11 : BaseDay
         answer = "TODO";
 
         return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 = '{answer}'");
+    }
+
+    private static ulong[] Blink(ulong number)
+    {
+        if (number == 0)
+        {
+            return [1];
+        }
+
+        var numberOfDigits = Math.Floor(Math.Log10(number)) + 1;
+        if (numberOfDigits % 2 == 0)
+        {
+            ulong divisor = (ulong)Math.Pow(10, numberOfDigits / 2);
+            return [number / divisor, number % divisor];
+        }
+
+        return [number * 2024];
     }
 }
