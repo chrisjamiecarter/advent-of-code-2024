d5cb456ff270f6d58247d8cfa63fd8bdabc25a5d
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-12-14 13:51:17 +0000

    Day eight part one complete

--- src/AdventOfCode2024/Solutions/Day08.cs
+++ src/AdventOfCode2024/Solutions/Day08.cs
@@ -4,7 +4,11 @@ namespace AdventOfCode2024.Solutions;
 
 public class Day08 : BaseDay
 {
-    private readonly string _input;
+    private readonly string _input = string.Empty;
+
+    private int _maxX = 0;
+
+    private int _maxY = 0;
 
     public Day08()
     {
@@ -13,23 +17,72 @@ public class Day08 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        var answer = string.Empty;
+        var antennas = ParseAntennas(_input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries));
+        var antinodes = new HashSet<(int x, int y)>();
 
-        // SOLVE.
+        foreach (var (key, positions) in antennas)
+        {
+            foreach (var (x, y) in positions)
+            {
+                foreach (var (prevX, prevY) in positions)
+                {
+                    if ((x, y) == (prevX, prevY)) continue;
 
-        answer = "TODO";
+                    var dX = x - prevX;
+                    var dY = y - prevY;
 
+                    AddAntinode(antinodes, x + dX, y + dY);
+                    AddAntinode(antinodes, prevX - dX, prevY - dY);
+                }
+            }
+        }
+
+        var answer = antinodes.Count;
         return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1 = '{answer}'");
     }
 
     public override ValueTask<string> Solve_2()
     {
-        var answer = string.Empty;
-
-        // SOLVE.
-
-        answer = "TODO";
-
+        var answer = "TODO";
         return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 = '{answer}'");
+    }
+
+    private Dictionary<char, List<(int x, int y)>> ParseAntennas(string[] lines)
+    {
+        _maxY = lines.Length;
+        _maxX = lines.First().Length;
+
+        var antennas = new Dictionary<char, List<(int x, int y)>>();
+
+        for (int y = 0; y < _maxY; y++)
+        {
+            for (int x = 0; x < _maxX; x++)
+            {
+                var location = lines[y][x];
+                if (location == '.') continue;
+
+                if (!antennas.ContainsKey(location))
+                {
+                    antennas[location] = [];
+                }
+
+                antennas[location].Add((x, y));
+            }
+        }
+
+        return antennas;
+    }
+
+    private void AddAntinode(HashSet<(int x, int y)> antinodes, int x, int y)
+    {
+        if (IsWithinRange(x, y))
+        {
+            antinodes.Add((x, y));
+        }
+    }
+
+    private bool IsWithinRange(int x, int y)
+    {
+        return x >= 0 && x < _maxX && y >= 0 && y < _maxY;
     }
 }
