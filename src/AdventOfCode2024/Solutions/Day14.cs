fe91b36e24fbc69d503881eed9306d9b3d40ed43
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-12-14 13:25:26 +0000

    Scaffolded day fourteen

---
+++ src/AdventOfCode2024/Solutions/Day14.cs
@@ -0,0 +1,35 @@
+using AoCHelper;
+
+namespace AdventOfCode2024.Solutions;
+
+public class Day14 : BaseDay
+{
+    private readonly string _input;
+
+    public Day14()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var answer = string.Empty;
+
+        // SOLVE.
+
+        answer = "TODO";
+
+        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1 = '{answer}'");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var answer = string.Empty;
+
+        // SOLVE.
+
+        answer = "TODO";
+
+        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 = '{answer}'");
+    }
+}
