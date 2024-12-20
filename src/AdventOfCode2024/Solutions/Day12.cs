cfdebbcae745d85951de4f4e1f27e64c34dda760
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-12-20 13:24:00 +0000

    Finished day twelve part two

--- src/AdventOfCode2024/Solutions/Day12.cs
+++ src/AdventOfCode2024/Solutions/Day12.cs
@@ -1,53 +1,52 @@
-using System.Security;
 using AoCHelper;
-using Spectre.Console;
 
 namespace AdventOfCode2024.Solutions;
 
 public class Day12 : BaseDay
 {
     private readonly string _input;
-
     private readonly char[,] _grid;
-
     private readonly int _maxX;
-
     private readonly int _maxY;
 
-    private static IEnumerable<(int X, int Y)> _directions =
-    [
-        (0, -1),
-        (1, 0),
-        (0, 1),
-        (-1, 0)
-    ];
+    private static readonly int[,] _deltaMap = new int[4, 2] { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
 
     public Day12()
     {
         _input = File.ReadAllText(InputFilePath);
 
         var rows = _input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-        var grid = new char[rows.First().Length, rows.Length];
+        _grid = new char[rows.First().Length, rows.Length];
         for (int y = 0; y < rows.Length; y++)
         {
             for (int x = 0; x < rows[y].Length; x++)
             {
-                grid[x, y] = rows[y][x];
+                _grid[x, y] = rows[y][x];
             }
         }
 
-        _grid = grid;
         _maxX = _grid.GetLength(0);
         _maxY = _grid.GetLength(1);
     }
 
     public override ValueTask<string> Solve_1()
     {
-        var answer = string.Empty;
+        var answer = Solve(ProcessRegionPerimeter);
 
-        // SOLVE.
+        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1 = '{answer}'");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var answer = Solve(ProcessRegionPerimeterSections);
+
+        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 = '{answer}'");
+    }
+
+    private string Solve(Func<int, int, bool[,], int> processRegion)
+    {
         var visitedPlots = new bool[_maxX, _maxY];
-        var fencingCost = 0;
+        int fencingCost = 0;
 
         for (int y = 0; y < _maxY; y++)
         {
@@ -55,44 +54,44 @@ public class Day12 : BaseDay
             {
                 if (!visitedPlots[x, y])
                 {
-                    fencingCost += ProcessRegion(x, y, visitedPlots);
+                    fencingCost += processRegion(x, y, visitedPlots);
                 }
             }
         }
 
-        answer = fencingCost.ToString();
-
-        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1 = '{answer}'");
+        return fencingCost.ToString();
     }
 
-    public override ValueTask<string> Solve_2()
+    private int ProcessRegionPerimeter(int startX, int startY, bool[,] visitedPlots)
     {
-        var answer = string.Empty;
-
-        // SOLVE.
-
-        answer = "TODO";
-
-        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 = '{answer}'");
-    }
-
-    private int ProcessRegion(int startX, int startY, bool[,] visitedPlots)
-    {
-        List<(int X, int Y)> regionPlots = [(startX, startY)];
         int perimeter = 0;
+        var regionPlots = new List<(int X, int Y)>() { (startX, startY) };
 
         visitedPlots[startX, startY] = true;
+        
         perimeter = ExplorePerimeter(startX, startY, visitedPlots, regionPlots, perimeter);
 
         return regionPlots.Count * perimeter;
     }
 
+    private int ProcessRegionPerimeterSections(int startX, int startY, bool[,] visitedPlots)
+    {
+        var regionPlots = new List<(int X, int Y)>() { (startX, startY) };
+        var sections = new List<(int Orientation, int Axis, int Plot, int Vector)>();
+
+        visitedPlots[startX, startY] = true;
+
+        ExplorePerimeterSection(startX, startY, visitedPlots, regionPlots, sections);
+
+        return regionPlots.Count * CountSides(sections);
+    }
+
     private int ExplorePerimeter(int x, int y, bool[,] visitedPlots, List<(int X, int Y)> regionPlots, int perimeter)
     {
-        foreach (var direction in _directions)
+        for (int i = 0; i < 4; i++)
         {
-            int exploreX = x + direction.X;
-            int exploreY = y + direction.Y;
+            int exploreX = x + _deltaMap[i, 1];
+            int exploreY = y + _deltaMap[i, 0];
 
             if (IsWithinBounds(exploreX, exploreY))
             {
@@ -119,9 +118,79 @@ public class Day12 : BaseDay
         return perimeter;
     }
 
+    private void ExplorePerimeterSection(int x, int y, bool[,] visitedPlots, List<(int X, int Y)> regionPlots, List<(int Orientation, int Axis, int Plot, int Vector)> sections)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            int exploreX = x + _deltaMap[i, 1];
+            int exploreY = y + _deltaMap[i, 0];
+
+            if (IsWithinBounds(exploreX, exploreY))
+            {
+                if (_grid[exploreX, exploreY] == _grid[x, y])
+                {
+                    if (!visitedPlots[exploreX, exploreY])
+                    {
+                        visitedPlots[exploreX, exploreY] = true;
+                        regionPlots.Add((exploreX, exploreY));
+                        ExplorePerimeterSection(exploreX, exploreY, visitedPlots, regionPlots, sections);
+                    }
+                }
+                else
+                {
+                    AddPerimeterSection(sections, i, exploreX, exploreY);
+                }
+            }
+            else
+            {
+                AddPerimeterSection(sections, i, exploreX, exploreY);
+            }
+        }
+    }
+
     private bool IsWithinBounds(int x, int y)
     {
         return x >= 0 && x < _maxX && y >= 0 && y < _maxY;
     }
 
+    private static void AddPerimeterSection(List<(int Orientation, int Axis, int Plot, int Vector)> sections, int i, int x, int y)
+    {
+        if (_deltaMap[i, 0] != 0)
+        {
+            sections.Add((i % 2, y * 2 - _deltaMap[i, 0], x, i));
+        }
+        else
+        {
+            sections.Add((i % 2, x * 2 - _deltaMap[i, 1], y, i));
+        }
+    }
+
+    private static int CountSides(List<(int Orientation, int Axis, int Plot, int Vector)> sections)
+    {
+        var sortedSections = sections.OrderBy(section => section.Orientation)
+                                     .ThenBy(section => section.Axis)
+                                     .ThenBy(section => section.Plot)
+                                     .ThenBy(section => section.Vector)
+                                     .ToList();
+
+        var sides = 0;
+        int orientation = -1;
+        int axis = -2;
+        int plot = -2;
+        int direction = -1;
+        foreach (var section in sortedSections)
+        {
+            if (section.Orientation != orientation || section.Axis != axis || !(section.Plot == plot || section.Plot == plot + 1) || (section.Orientation == orientation && section.Vector != direction))
+            {
+                sides++;
+            }
+
+            orientation = section.Orientation;
+            axis = section.Axis;
+            plot = section.Plot;
+            direction = section.Vector;
+        }
+
+        return sides;
+    }
 }
