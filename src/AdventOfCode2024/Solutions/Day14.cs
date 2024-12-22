21495a5e89352b58c4633ebb486b4991ea59b206
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-12-22 21:33:47 +0000

    Completed day fourteen part one

--- src/AdventOfCode2024/Solutions/Day14.cs
+++ src/AdventOfCode2024/Solutions/Day14.cs
@@ -5,6 +5,8 @@ namespace AdventOfCode2024.Solutions;
 public class Day14 : BaseDay
 {
     private readonly string _input;
+    private readonly int _dimensionX = 101;
+    private readonly int _dimensionY = 103;
 
     public Day14()
     {
@@ -13,11 +15,39 @@ public class Day14 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        var answer = string.Empty;
+        var robots = new List<Robot>();
 
-        // SOLVE.
+        foreach (var line in _input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
+        {
+            var robotInitialiser = line.Split(' ');
+            var position = robotInitialiser[0].Replace("p=", string.Empty).Split(',').Select(int.Parse).ToArray();
+            var velocity = robotInitialiser[1].Replace("v=", string.Empty).Split(',').Select(int.Parse).ToArray();
 
-        answer = "TODO";
+            robots.Add(new Robot(position[0], position[1]).Move(velocity[0], velocity[1], _dimensionX, _dimensionY, 100));
+        }
+
+        var quadrants = new int[4];
+        foreach (var robot in robots)
+        {
+            if (robot.X < _dimensionX / 2 && robot.Y < _dimensionY / 2)
+            {
+                quadrants[0]++;
+            }
+            else if (robot.X > _dimensionX / 2 && robot.Y < _dimensionY / 2)
+            {
+                quadrants[1]++;
+            }
+            else if (robot.X < _dimensionX / 2 && robot.Y > _dimensionY / 2)
+            {
+                quadrants[2]++;
+            }
+            else if (robot.X > _dimensionX / 2 && robot.Y > _dimensionY / 2)
+            {
+                quadrants[3]++;
+            }
+        }    
+
+        var answer = quadrants.Aggregate((a, b) => a * b);
 
         return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1 = '{answer}'");
     }
@@ -31,5 +61,34 @@ public class Day14 : BaseDay
         answer = "TODO";
 
         return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 = '{answer}'");
+    }
+
+    private readonly record struct Robot(int X, int Y)
+    {
+        public Robot Move(int velocityX, int velocityY, int dimensionX, int dimensionY, int steps)
+        {
+            var positionX = X + (velocityX * steps) % dimensionX;
+            var positionY = Y + (velocityY * steps) % dimensionY;
+
+            if (positionX < 0)
+            {
+                positionX += dimensionX;
+            }
+            if (positionX >= dimensionX)
+            {
+                positionX -= dimensionX;
+            }
+
+            if (positionY < 0)
+            {
+                positionY += dimensionY;
+            }
+            if (positionY >= dimensionY)
+            {
+                positionY -= dimensionY;
+            }
+
+            return new Robot(positionX, positionY);
+        }
     }
 }
